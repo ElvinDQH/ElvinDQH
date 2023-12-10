790d6f3257e6a1b10babdd432cb7e34376a99b64
ElvinDQH <doqhao@gmail.com>
2023-12-10 20:45:00 +0800

    style: change display message when selecting city

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input('Would you like to see data for Chicago, New York City, or Washington?\n').lower()
+        city = input('Would you like to see data for Chicago, New York City, or Washington? More data is coming up on the next release - stay tune!\n').lower()
         if city in ['chicago', 'new york city', 'washington']:
             break
         else:
